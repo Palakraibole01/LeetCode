0898b4059d9ccd98591a54abdfcc364bbe91506f
Palakraibole01 <palakraibole@gmail.com>
2026-07-31 14:21:33 +0530

    Time: 94 ms (72.46%), Space: 68.9 MB (97.33%) - LeetHub

---
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maximumRemovals(String s, String p, int[] removable) {
+        int start = 0, end = removable.length;
+        int[] map = new int[s.length()];
+        Arrays.fill(map, removable.length);
+        for(int i = 0; i < removable.length; ++i){
+            map[removable[i]] = i;
+        }
+        while(start < end){
+            int mid = (start + end + 1) / 2;
+            int j = 0;
+            for(int i = 0; i < s.length() && j < p.length(); ++i)
+                if(map[i] >= mid && s.charAt(i) == p.charAt(j))
+                    ++j;
+                
+            if(j == p.length())
+                start = mid;
+            else
+                end = mid - 1;
+            
+            
+        }
+        return start;
+    }
+}
